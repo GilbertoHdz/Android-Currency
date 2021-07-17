260e8c9b20ee35b6bbb2d693f67776919fa329d3
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2021-07-16 21:41:10 -0500

    feature: :component: as core provider
    * Create component module to handle styles and whole project sharing resources such extensions or libraries

--- buildSrc/src/main/java/Plugins.kt
+++ buildSrc/src/main/java/Plugins.kt
@@ -5,6 +5,7 @@ object Plugins {
 
     // Module Level
     const val ANDROID_APPLICATION_PLUGIN = "com.android.application"
+    const val ANDROID_LIBRARY_PLUGIN = "com.android.library"
     const val KOTLIN_ANDROID_PLUGIN = "kotlin-android"
     const val KOTLIN_KAPT_PLUGIN = "kotlin-kapt"
 }
---
+++ buildSrc/src/main/java/extensions/DependencyHandlerExtensions.kt
@@ -0,0 +1,35 @@
+package extensions
+
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+
+/**
+ * Adds dependencies to libraries module
+ */
+fun DependencyHandler.addCoreLibraryModuleDependencies() {
+    implementation(Deps.KOTLIN)
+    implementation(Deps.ANDROIDX_CORE_KTX)
+
+    // Support and Widgets
+    implementation(Deps.APPCOMPAT)
+    implementation(Deps.MATERIAL)
+    implementation(Deps.CONSTRAINT_LAYOUT)
+
+    // Lifecycle, LiveData, ViewModel
+    implementation(Deps.LIFECYCLE_LIVEDATA_KTX)
+    implementation(Deps.LIFECYCLE_VIEWMODEL_KTX)
+}
+
+/*
+ * These extensions mimic the extensions that are generated on the fly by Gradle.
+ * They are used here to provide above dependency syntax that mimics Gradle Kotlin DSL
+ * syntax in module\build.gradle.kts files.
+ */
+@Suppress("detekt.UnusedPrivateMember")
+private fun DependencyHandler.implementation(dependencyNotation: Any): Dependency? =
+    add("implementation", dependencyNotation)
+
+@Suppress("detekt.UnusedPrivateMember")
+private fun DependencyHandler.kapt(dependencyNotation: Any): Dependency? =
+    add("kapt", dependencyNotation)
