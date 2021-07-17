e0aa649d34e61ae494e0b4d572eeb753e56ea61b
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2021-07-16 21:19:44 -0500

    task: setup :buildSrc:
    * Config library and version on buildSrc to handle whole project libraries

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,6 @@
+object Plugins {
+    // Project Level
+    const val CLASSPATH_GRADLE = "com.android.tools.build:gradle:${PluginVersion.GRADLE_VERSION}"
+    const val CLASSPATH_KOTLIN_GRADLE = "org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginVersion.KOTLIN_VERSION}"
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,4 @@
+object PluginVersion {
+    const val KOTLIN_VERSION = "1.4.10"
+    const val GRADLE_VERSION = "4.1.0"
+}
