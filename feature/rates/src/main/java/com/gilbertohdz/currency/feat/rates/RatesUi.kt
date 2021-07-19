9b3544e865706f1fe0eb36af01b5560088262985
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2021-07-19 00:20:40 -0500

    fix: rates UI constraint references and decimal format to two places

--- feature/rates/src/main/java/com/gilbertohdz/currency/feat/rates/RatesUi.kt
+++ feature/rates/src/main/java/com/gilbertohdz/currency/feat/rates/RatesUi.kt
@@ -1,14 +1,16 @@
 package com.gilbertohdz.currency.feat.rates
 
 import com.gilbertohdz.currency.lib.utils.common.ErrorTypeCommon
+import java.text.DecimalFormat
 
 data class RatesUi(
   val currency: String,
   val value: Double,
   val toConvert: Int
 ) {
-  fun convertToValue(): Double {
-    return this.value * this.toConvert
+  fun convertToValue(): String {
+    val decimalFormat = DecimalFormat("0.00")
+    return decimalFormat.format((this.value * this.toConvert))
   }
 }
 
