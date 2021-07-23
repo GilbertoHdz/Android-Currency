ff489e1f80776acb203524a018dc902dd05df94c
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2021-07-22 23:37:17 -0500

    feat: creating :data: module
    * Creating module data to include prefs and persistence dao interface used in the whole project as a data management

---
+++ libraries/data/build.gradle.kts
@@ -0,0 +1,52 @@
+
+plugins {
+    id(Plugins.ANDROID_LIBRARY_PLUGIN)
+    id(Plugins.KOTLIN_ANDROID_PLUGIN)
+    id(Plugins.KOTLIN_ANDROID_EXTENSIONS_PLUGIN)
+    id(Plugins.KOTLIN_KAPT_PLUGIN)
+    id(Plugins.DAGGER_HILT_PLUGIN)
+}
+
+android {
+    compileSdkVersion(AndroidVersion.COMPILE_SDK_VERSION)
+    
+    defaultConfig {
+        minSdkVersion(AndroidVersion.MIN_SDK_VERSION)
+        targetSdkVersion(AndroidVersion.TARGET_SDK_VERSION)
+        versionCode = AndroidVersion.VERSION_CODE
+        versionName = AndroidVersion.VERSION_NAME
+    
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += mapOf(
+                    "room.schemaLocation" to "$projectDir/schemas",
+                    "room.incremental" to "true",
+                    "room.expandProjection" to "true"
+                )
+            }
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    
+    implementation(Deps.KOTLIN)
+    implementation(Deps.RX_KOTLIN)
+    
+    // Dagger Hilt
+    implementation(Deps.DAGGER_HILT_ANDROID)
+    kapt(Deps.DAGGER_HILT_COMPILER)
+    
+    // ROOM DB
+    implementation(Deps.ROOM_DB)
+    kapt(Deps.ROOM_DB_COMPILER)
+}
